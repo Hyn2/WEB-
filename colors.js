92330088f211c8f69c6c40271d369f14274637ba
Hyn2 <125263770+Hyn2@users.noreply.github.com>
2023-02-20 09:14:43 +0900

    끝

---
+++ colors.js
@@ -0,0 +1,37 @@
+function setColor(color) {
+  var alist = document.querySelectorAll("a");
+  var i = 0;
+  while (i < alist.length) {
+    alist[i].style.color = color;
+    i = i + 1;
+  }
+}
+
+var body = {
+  background: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
+  font: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+};
+
+function nightDayHandler(self) {
+  var target2 = document.querySelector("h1");
+  var target3 = document.querySelector("ol");
+  if (self.value === "NIGHT") {
+    body.background("black");
+    body.font("white");
+    target2.style.borderBottom = "5px solid white";
+    target3.style.borderRight = "5px solid white";
+    setColor("white");
+    self.value = "DAY";
+  } else {
+    body.background("white");
+    body.font("black");
+    target2.style.borderBottom = "5px solid black";
+    target3.style.borderRight = "5px solid black";
+    setColor("black");
+    self.value = "NIGHT";
+  }
+}
